8023dfd809e7ab385fa93aa58ef3058ecbd90232
feronova <33385265+feronova@users.noreply.github.com>
2017-11-05 10:44:23 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
-const client = new Discord.client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
-	console.log('I am ready');
+    console.log('Bot is ready!');
 });
 
-client.on('message', message => {
-	if(message.content === 'ping') {
-		message.reply('pong');
+client.on('message' , (message) => {
+	if (message.content == '!tes') {
+			message.channel.send("tis!", {
+			tts:false
+			})
 	}
-)};
-
+});
 client.login(process.env.BOT_TOKEN);
