fcb9f13a40347e06dfd64b69c02784de7da55c83
feronova <33385265+feronova@users.noreply.github.com>
2017-11-06 21:36:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,32 +14,32 @@ client.on('message' , (message) => {
 	}
 	
 	if (input == 'BACOT') {
-			message.channel.send("**bacot**, bad word, forbidden!", {
+			message.channel.send("bacot, bad word, forbidden!", {
 			tts:false
 			})
 	}
 	if (input == 'BACOD') {
-			message.channel.send("**bacod**, bad word, forbidden!", {
+			message.channel.send("bacod, bad word, forbidden!", {
 			tts:false
 			})
 	}
 	if (input == 'BCT') {
-			message.channel.send("**bct**, bad word, forbidden!", {
+			message.channel.send("bct, bad word, forbidden!", {
 			tts:false
 			})
 	}
 	if (input == 'BCD') {
-			message.channel.send("**bcd**, bad word, forbidden!", {
+			message.channel.send("bcd, bad word, forbidden!", {
 			tts:false
 			})
 	}
 	if (input == 'LOL XD' ) {
-			message.channel.send("**LoL XD** = lambang ketawa yang biasa dipakai otaku (tapi orang biasa juga suka)", {
+			message.channel.send("LoL XD = lambang ketawa yang biasa dipakai otaku (tapi orang biasa juga suka)", {
 			tts:false
 			})
 	}
 	if (input == 'DED' ) {
-			message.channel.send("ded, bad word, forbidden!! **VERY FORBIDDEN**.", {
+			message.channel.send("ded, bad word, forbidden!!", {
 			tts:false
 			})
 	}
@@ -49,17 +49,17 @@ client.on('message' , (message) => {
 			})
 	}
 	if (input == 'TOLO' ) {
-			message.channel.send("**tolo**, bad word, forbidden!! **VERY FORBIDDEN**.", {
+			message.channel.send("tolo, bad word, forbidden!!", {
 			tts:false
 			})
 	}
 	if (input == 'GAJELAS' ) {
-			message.channel.send("gajelas, bad word, forbidden!! **VERY FORBIDDEN**.", {
+			message.channel.send("gajelas, bad word, forbidden!!", {
 			tts:false
 			})
 	}
 	if (input.indexOf('@EVERYONE') != -1 ) {
-			message.channel.send("no tag, you **DICK**, annoying!", {
+			message.channel.send("no tag, you DICK, annoying!", {
 			tts:false
 			})
 	}
