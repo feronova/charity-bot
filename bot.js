0fb98ca11082ff71b64e586d57e0b6a9142d609e
feronova <33385265+feronova@users.noreply.github.com>
2017-11-06 21:54:21 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    	console.log('Bot is ready!');
 	client.user.setGame("pinDICK XD");
 });
 client.on('message' , (message) => {
